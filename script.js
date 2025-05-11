e60a32ca985658c8e04b6173b0bd7ccca00752e4
xKuraido <81272935+KuraidoXenpaI@users.noreply.github.com>
2025-05-11 22:29:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let webFlames = document.getElementById("webflames");
+    webFlames.addEventListener("click", function () {
+        window.open("https://github.com/KuraidoXenpaI/web-flames", "_blank");
+    });
+
+    let spaceShooter = document.getElementById("spaceshooter");
+    spaceShooter.addEventListener("click", function () {
+        window.open("https://github.com/KuraidoXenpaI/Space-Shooter", "_blank");
+    });
+
+    let curiousCpe = document.getElementById("curiouscpe");
+    curiousCpe.addEventListener("click", function() {
+        window.open("https://github.com/KuraidoXenpaI/CuriousCpE", "_blank");
+    });
+
+    let snake = document.getElementById("snake");
+    snake.addEventListener("click", function() {
+        window.open("https://github.com/KuraidoXenpaI/Java-SnakeGame", "_blank");
+    });
+});
